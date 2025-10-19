37780e870cda36b7eed70ccbcc5c720216d61fb5
berk <118214474+littleborek@users.noreply.github.com>
2025-10-20 00:48:56 +0300

    delete

--- src/main/java/com/example/chatapp/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.chatapp.repository;
-
-import com.example.chatapp.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    User findByUsername(String username);
-}
